d9b1c00ecb724a1d72659c74ba8e5aed94ac73ef
Bea DelRosario <bdelronyc@Beas-MacBook-Pro.local>
2020-08-24 16:32:52 -0700

    private route added

--- app/src/components/HowToCard.js
+++ app/src/components/HowToCard.js
@@ -4,11 +4,9 @@ export const HowToCard = () => {
   return (
   <div>
     {howTo && <container>
-        <h2>{howTo.name}</h2>
-        <h3>{howTo.description}</h3>
+        <h2>{howTo.title}</h2>
         <h3>{howTo.category}</h3>
-        <h3>{howTo.complexity}</h3>
-        <p>{howTo.steps}</p>
+        <p>{howTo.content}</p>
     </container>}
   </div>
   )
