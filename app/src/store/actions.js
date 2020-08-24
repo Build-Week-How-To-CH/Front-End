1612f80ed10dc894dd213d724548da66fa7ba0ba
Galo Sandoval <galosan92@gmail.com>
2020-08-24 13:45:53 -0700

    added login form

--- app/src/store/actions.js
+++ app/src/store/actions.js
@@ -17,3 +17,19 @@ export const fetchHowTos = (id) => {
       .catch((err) => console.log(err, 'teehee'));
   };
 };
+
+export const signUp = client => {
+  return dispatch => {
+    dispatch({type: LOGIN_START})
+    axiosWithAuth()
+    .post('', client)//need an endpoint, might need 2, one for creator, one for user
+    .then(res => {
+      console.log(res)
+      dispatch({type: FETCH_LOGIN_SUCCESS, payload: res.data})
+      localStorage.setItem('token', res.token)
+    })
+    .catch(err => err)
+  }
+}
+
+
--- app/src/store/reducers.js
+++ app/src/store/reducers.js
@@ -1,16 +1,22 @@
-import { FETCH_HOW_TO_LOGIN, FETCH_HOW_TO_SUCCESS } from "./actions";
+import { LOGIN_START, FETCH_LOGIN_SUCCESS } from "./actions";
 
 const initialState = {
   isLoading: false,
   data: "",
-  error: "",
 };
 
-
-
 const reducer = (state = initialState, action) => {
-  switch(action.type){
-  case FETCH_HOW_TO_LOGIN:
-    return 
+  switch (action.type) {
+    case LOGIN_START:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case FETCH_LOGIN_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        data: action.payload,
+      };
   }
 };
