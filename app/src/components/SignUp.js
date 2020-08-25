65a5cb4b291ea5e131d2aa0d5983990e650c609c
Galo Sandoval <galosan92@gmail.com>
2020-08-25 14:35:22 -0700

    fixed bugs

--- app/src/components/SignUp.js
+++ app/src/components/SignUp.js
@@ -1,7 +1,21 @@
 import React from "react";
 import { NavLink, Route, useHistory } from "react-router-dom";
-import styled from 'styled-components';
+import styled from "styled-components";
 
+// Styling
+
+const StyledDiv = styled.div`
+  background-color: rgb(232, 232, 228);
+  border-bottom: 2px solid black;
+`;
+
+const StyledLink = styled.div`
+  margin-bottom: 1%;
+`;
+
+const StyledHeading = styled.h4`
+  font-family: "Cherry Swash";
+`;
 
 function SignUp(props) {
   const { values, submit, inputChange, disabled, errors } = props;
@@ -20,76 +34,67 @@ function SignUp(props) {
 
   // Event Listeners
 
-  function changeColor (e){
-    e.target.style.color = '#000099';
+  function changeColor(e) {
+    e.target.style.color = "#000099";
   }
 
-  function changeColorBack (e){
-    e.target.style.color = 'black';
+  function changeColorBack(e) {
+    e.target.style.color = "black";
   }
-
-  // Styling
-  
-  const StyledDiv = styled.div`
-    background-color:	rgb(232, 232, 228);
-    border-bottom:2px solid black;
-  `
-
-  const StyledLink = styled.div`
-  margin-bottom:1%;
-  `
-
-  const StyledHeading = styled.h4`
-  font-family:'Cherry Swash';
-  `
-
-
-
 
   return (
     <Route path="/signup">
       <StyledDiv>
-          <NavLink to="/" onMouseOver={changeColor} onMouseOut={changeColorBack}>Home</NavLink>
-          <form className="form container" onSubmit={onSubmit}>
-            <div className="errors">
-              <div>{errors.email}</div>
-              <div>{errors.username}</div>
-              <div>{errors.password}</div>
-            </div>
+        <NavLink to="/" onMouseOver={changeColor} onMouseOut={changeColorBack}>
+          Home
+        </NavLink>
+        <form className="form container" onSubmit={onSubmit}>
+          <div className="errors">
+            <div>{errors.email}</div>
+            <div>{errors.username}</div>
+            <div>{errors.password}</div>
+          </div>
 
-            <div className="inputs">
-              <StyledHeading><h4>Sign Up</h4></StyledHeading>
+          <div className="inputs">
+            <StyledHeading>
+              <h4>Sign Up</h4>
+            </StyledHeading>
 
-              <label>
-                Username:&nbsp;
-                <input
-                  value={values.username}
-                  onChange={onInputChange}
-                  name="username"
-                  type="text"
-                />
-              </label>
+            <label>
+              Username:&nbsp;
+              <input
+                value={values.username}
+                onChange={onInputChange}
+                name="username"
+                type="text"
+              />
+            </label>
 
-              <label>
-                Password:&nbsp;
-                <input
-                  value={values.password}
-                  onChange={onInputChange}
-                  name="password"
-                  type="password"
-                />
-              </label>
+            <label>
+              Password:&nbsp;
+              <input
+                value={values.password}
+                onChange={onInputChange}
+                name="password"
+                type="password"
+              />
+            </label>
 
-              <button disabled={disabled}>Sign me up!</button>
-              <br></br>
-              <StyledLink>
-                <div className="makeLink" onClick={() => history.push("/")} onMouseOver = {changeColor} onMouseOut = {changeColorBack}>
-                  Already have an account? Click to log in.
-                </div>
-              </StyledLink>
-            </div>
-          </form>
-        </StyledDiv>
+            <button disabled={disabled}>Sign me up!</button>
+            <br></br>
+            <StyledLink>
+              <div
+                className="makeLink"
+                onClick={() => history.push("/")}
+                onMouseOver={changeColor}
+                onMouseOut={changeColorBack}
+              >
+                Already have an account? Click to log in.
+              </div>
+            </StyledLink>
+          </div>
+        </form>
+      </StyledDiv>
     </Route>
   );
 }
