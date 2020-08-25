fe3edd182575ca21e2d75e4b21b9450c573eec21
Galo Sandoval <galosan92@gmail.com>
2020-08-25 08:45:12 -0700

    modifying actions

--- app/src/store/actions.js
+++ app/src/store/actions.js
@@ -3,12 +3,12 @@ import { axiosWithAuth } from "../utils/axiosWithAuth";
 export const LOGIN_START = 'LOGIN_START'
 export const FETCH_LOGIN_SUCCESS = 'FETCH_LOGIN_SUCCESS'
 
-export const fetchHowTos = (id) => {
+export const fetchHowTos = () => {
   return (dispatch) => {
     dispatch({ type: LOGIN_START });
 
     axiosWithAuth()
-      .get(`${id}`)//need to add an endpoint
+      .get(`/api/howtos`)//need to add an endpoint
       .then((res) => {
         console.log(res);
         dispatch({ type: FETCH_LOGIN_SUCCESS, payload: res.data });
@@ -22,7 +22,7 @@ export const signUp = client => {
   return dispatch => {
     dispatch({type: LOGIN_START})
     axiosWithAuth()
-    .post('', client)//need an endpoint, might need 2, one for creator, one for user
+    .post('/api/auth/register', client)//need an endpoint, might need 2, one for creator, one for user
     .then(res => {
       console.log(res)
       dispatch({type: FETCH_LOGIN_SUCCESS, payload: res.data})
